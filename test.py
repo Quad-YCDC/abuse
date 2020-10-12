1b00f06fd658986fdfc811676a13e37999197c86
jongsangkuun <tmclzns@gmail.com>
2020-10-12 11:55:24 +0900

    불필요한 데이터 제거

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
-#psql 연동 
+import requests, json, datetime
+from time import sleep
 import psycopg2
 
-conn = psycopg2.connect(host='localhost',dbname='testdb1',user='tmclzns',password='ycdc@2020!',port='5432')
-cur = conn.cursor()
 
-data_format = '%Y-%m-%d %H:%M:%S'
+url_id = 'https://urlhaus-api.abuse.ch/v1/urlid/'
 
-def reputation_audit_start():
-    cur.execute('select url_id from reputation_audit_abuse order by log_date desc limit 1;')
-    
-    last_urlid = cur.fetchall()
-    last_urlid = list(map(int,last_urlid[0]))[0]
-    print(type(last_urlid))
-    cur.execute('insert into reputation_audit_abuse(audit_log,log_date,url_id) values(\'abuse 수집 시작\' ,now(),%d);'%last_urlid)
-    conn.commit()
-    return last_urlid
-    
 
-last_urlid = reputation_audit_start()
-print(last_urlid)
+for urlid_key in range (7999,8000):
+    params = {'urlid':urlid_key} 
+    res_csv = requests.post(url_id,data=params)
+    print(res_csv.text)
+    if res_csv.text == '':
+        print(1)
 
-now_date = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-cur.execute('insert into reputation_data(service,indicator_type,indicator,reg_date,cre_date) values(2,1,%s,%s,%s);',(res_csv_json['url'],now_date,res_csv_json['date_added']))
+    else:
+        print(2)
+    res_csv_json = json.loads(res_csv.text)
